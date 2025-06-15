d49513ab76672b6028ac211000e7230e4ce38ea7
Jim peregoy <jim.peregoy@gmail.com>
2025-06-15 09:13:09 -0400

    add search feature

---
+++ maps/javascripts/locations.js
@@ -0,0 +1,495 @@
+﻿var locations = [
+	{
+        name: "Scarlet Hill",
+        lng: -71.54862284660341,
+        lat: 42.60999047719178
+    }, {
+        name: "Academy Hill",
+        lng: -71.63085938,
+        lat: 42.63260555312694
+    }, {
+        name: "Allen Conservation Area",
+        lng: -71.60820007,
+        lat: 42.60433196319414
+    }, {
+        name: "Angus Acre",
+        lng: -71.54977083,
+        lat: 42.61273077064815
+    }, {
+        name: "Baddacook Conservation Land",
+        lng: -71.53764725,
+        lat: 42.62582305564919
+    }, {
+        name: "Baddacook Pond Access",
+        lng: -71.53040528,
+        lat: 42.62347864592216
+    }, {
+        name: "Baddacook Pond Conservation Area",
+        lng: -71.52848482,
+        lat: 42.62489129
+    }, {
+        name: "Baddacook Woods",
+        lng: -71.53033018,
+        lat: 42.607461295927926
+    }, {
+        name: "Baddocook Woods",
+        lng: -71.52902126,
+        lat: 42.60813402996914
+    }, {
+        name: "Bates Land",
+        lng: -71.56552076,
+        lat: 42.59460161
+    }, {
+        name: "Batten Woods",
+        lng: -71.50891542,
+        lat: 42.58047860691505
+    }, {
+        name: "Bernier-Bissell Property",
+        lng: -71.64317608,
+        lat: 42.63009063828811
+    }, {
+        name: "Best-Fitch Woods",
+        lng: -71.56335354,
+        lat: 42.6303469
+    }, {
+        name: "Blackman Field and Woods",
+        lng: -71.54758215,
+        lat: 42.58672547478246
+    }, {
+        name: "Blackman Land",
+        lng: -71.56297803,
+        lat: 42.59046180337408
+    }, {
+        name: "Boat House Road/Bedigian",
+        lng: -71.52067423,
+        lat: 42.60045912711983
+    }, {
+        name: "Brookfield Commons",
+        lng: -71.49776816,
+        lat: 42.58140138844037
+    }, {
+        name: "Brookside Place Conservation Area",
+        lng: -71.55189514,
+        lat: 42.604516425069804
+    }, {
+        name: "Bruner Land",
+        lng: -71.51571751,
+        lat: 42.63200601
+    }, {
+        name: "Carmichael Swamp Conservation Area",
+        lng: -71.50391579,
+        lat: 42.58210536967923
+    }, {
+        name: "Carmichael Swamp Conservation Area II",
+        lng: -71.50814295,
+        lat: 42.58303718554104
+    }, {
+        name: "Charles Bixby Conservation Area",
+        lng: -71.61705136,
+        lat: 42.60076869048858
+    }, {
+        name: "Cold Spring Pond Conservation Area",
+        lng: -71.57073498,
+        lat: 42.64315085815801
+    }, {
+        name: "Cow Pond Brook Conservation Area",
+        lng: -71.50816441,
+        lat: 42.61404409026237
+    }, {
+        name: "Cow Pond Brook Park",
+        lng: -71.50447369,
+        lat: 42.62045267926206
+    }, {
+        name: "Cronin Conservation Area",
+        lng: -71.57418966,
+        lat: 42.64441297
+    }, {
+        name: "Cronin Land",
+        lng: -71.50749922,
+        lat: 42.61779934266955
+    }, {
+        name: "Cronin-Massapoag Conservation Area",
+        lng: -71.49932384,
+        lat: 42.634180848718174
+    }, {
+        name: "David Norris Property",
+        lng: -71.61987305,
+        lat: 42.62894467353837
+    }, {
+        name: "Deerhaven Conservation Area",
+        lng: -71.51216626,
+        lat: 42.61141957026883
+    }, {
+        name: "Deerhaven Conservation Restriction",
+        lng: -71.51291728,
+        lat: 42.61550842540575
+    }, {
+        name: "Duck Pond Conservation Area",
+        lng: -71.51512742,
+        lat: 42.594259913730426
+    }, {
+        name: "Eldridge Conservation Area",
+        lng: -71.5176487,
+        lat: 42.59701661185372
+    }, {
+        name: "Eliades Conservation Area",
+        lng: -71.57722592,
+        lat: 42.58038135439906
+    }, {
+        name: "Farmers And Mechanics Club",
+        lng: -71.60139799,
+        lat: 42.61892881256669
+    }, {
+        name: "Fitch's Bridge Estates",
+        lng: -71.60891891,
+        lat: 42.622711365018546
+    }, {
+        name: "Fitch-Woods",
+        lng: -71.5593195,
+        lat: 42.63589782715597
+    }, {
+        name: "Flat Pond",
+        lng: -71.64957047,
+        lat: 42.641511359385184
+    }, {
+        name: "Flavell Crossing Conservation Area",
+        lng: -71.50138378,
+        lat: 42.59850872337905
+    }, {
+        name: "Flavell Road Conservation Area",
+        lng: -71.50263906,
+        lat: 42.608783204945574
+    }, {
+        name: "Fletcher Hill Conservation Area",
+        lng: -71.51361465,
+        lat: 42.57423965719312
+    }, {
+        name: "Floyd Conservation Area",
+        lng: -71.53771162,
+        lat: 42.63336472
+    }, {
+        name: "Fritz and Helen Walker Conservation Area",
+        lng: -71.55601501,
+        lat: 42.63239845055963
+    }, {
+        name: "Gamlin Crystal Spring Conservation Area",
+        lng: -71.51297092,
+        lat: 42.630079569030855
+    }, {
+        name: "Gamlin Picnic Site",
+        lng: -71.60020709,
+        lat: 42.625118274356026
+    }, {
+        name: "Genthner Woods",
+        lng: -71.54286146,
+        lat: 42.58596695637394
+    }, {
+        name: "Gibbet Hill",
+        lng: -71.5598774,
+        lat: 42.609099115130526
+    }, {
+        name: "Gilson Conservation Area",
+        lng: -71.54509306,
+        lat: 42.59581117571544
+    }, {
+        name: "Gilson Estates Conservation Area",
+        lng: -71.54524326,
+        lat: 42.60140416891425
+    }, {
+        name: "Groton Hills Conservation Area",
+        lng: -71.54193878,
+        lat: 42.627039062879305
+    }, {
+        name: "Groton Woods Conservation Area",
+        lng: -71.5073061,
+        lat: 42.59197734901087
+    }, {
+        name: "Half Moon Swamp",
+        lng: -71.55895472,
+        lat: 42.582459222693714
+    }, {
+        name: "Harrison S. Ripley Forest",
+        lng: -71.5305233,
+        lat: 42.63266081349691
+    }, {
+        name: "Hawtree Conservation Area",
+        lng: -71.53981447,
+        lat: 42.63855047379635
+    }, {
+        name: "Hemlock Grove",
+        lng: -71.62096739,
+        lat: 42.60791673462633
+    }, {
+        name: "Hurd Property",
+        lng: -71.5605855,
+        lat: 42.59337961334887
+    }, {
+        name: "Integrity Way Conservation Area",
+        lng: -71.6230917,
+        lat: 42.609313874405274
+    }, {
+        name: "J. Harry Rich State Forest",
+        lng: -71.58124924,
+        lat: 42.63780014069888
+    }, {
+        name: "James River Greenway",
+        lng: -71.59384489,
+        lat: 42.62582545142768
+    }, {
+        name: "Johnston Conservation Area",
+        lng: -71.60983086,
+        lat: 42.61883767249933
+    }, {
+        name: "June's Wood",
+        lng: -71.61562443,
+        lat: 42.62270792756377
+    }, {
+        name: "Kemp Woods",
+        lng: -71.61549568,
+        lat: 42.62237979852516
+    }, {
+        name: "Keyes Conservation Easement",
+        lng: -71.62236214,
+        lat: 42.618465793658395
+    }, {
+        name: "Knowles Siding Conservation Area",
+        lng: -71.57734394,
+        lat: 42.60874336989885
+    }, {
+        name: "Labbe Road Conservation Area",
+        lng: -71.49925411,
+        lat: 42.58313654871187
+    }, {
+        name: "Lawrence Land",
+        lng: -71.6331768,
+        lat: 42.62951275517449
+    }, {
+        name: "Lawrence Park Conservation Area",
+        lng: -71.62054896,
+        lat: 42.606181615001454
+    }, {
+        name: "Lawrence Woods",
+        lng: -71.59388781,
+        lat: 42.605484878595966
+    }, {
+        name: "Longley Estates I Conservation Area",
+        lng: -71.57491922,
+        lat: 42.64733149429362
+    }, {
+        name: "Longley Estates II Conservation Area",
+        lng: -71.57165766,
+        lat: 42.63650825434283
+    }, {
+        name: "Lost Lake Recreation Area",
+        lng: -71.52007341,
+        lat: 42.59284801603098
+    }, {
+        name: "Lotz Land",
+        lng: -71.55736685,
+        lat: 42.58392892641961
+    }, {
+        name: "Martin's Pond Brook",
+        lng: -71.55425549,
+        lat: 42.61288872821615
+    }, {
+        name: "Mason Back 100",
+        lng: -71.5115118,
+        lat: 42.63667993566916
+    }, {
+        name: "Mccarthy Drive",
+        lng: -71.50342226,
+        lat: 42.60734658524711
+    }, {
+        name: "Mclains Woods Conservation Area",
+        lng: -71.5566802,
+        lat: 42.63629257340087
+    }, {
+        name: "Meadowbrook Conservation Area",
+        lng: -71.5631175,
+        lat: 42.65515852337308
+    }, {
+        name: "Meredith Bissell Property",
+        lng: -71.64364815,
+        lat: 42.63423308910875
+    }, {
+        name: "Milton Starr Land",
+        lng: -71.65671587,
+        lat: 42.64663918
+    }, {
+        name: "Mountain Lakes Trail",
+        lng: -71.51702106,
+        lat: 42.59834463597892
+    }, {
+        name: "Northwoods Conservation Area",
+        lng: -71.56270981,
+        lat: 42.64685505284199
+    }, {
+        name: "Old Mill Conservation Area",
+        lng: -71.62524819,
+        lat: 42.59422115908872
+    }, {
+        name: "Perry Land",
+        lng: -71.56135798,
+        lat: 42.636656070229016
+    }, {
+        name: "Petapawag Canoe Launch",
+        lng: -71.59283638,
+        lat: 42.62790849645806
+    }, {
+        name: "Peter E. Bertozzi Conservation Area",
+        lng: -71.65617943,
+        lat: 42.63540476077195
+    }, {
+        name: "Phebe R Keyes Woods",
+        lng: -71.57659292,
+        lat: 42.59352679784603
+    }, {
+        name: "Priest Family Conservation Area",
+        lng: -71.5601778,
+        lat: 42.61895378886161
+    }, {
+        name: "Red Line",
+        lng: -71.50138378,
+        lat: 42.63348722094674
+    }, {
+        name: "Reedy Meadow Road",
+        lng: -71.55592918,
+        lat: 42.64063461682905
+    }, {
+        name: "Reynolds Tract",
+        lng: -71.56000614,
+        lat: 42.58484549829862
+    }, {
+        name: "Richard F.Priest Memorial Area",
+        lng: -71.51861429,
+        lat: 42.583779469566764
+    }, {
+        name: "Riley Land",
+        lng: -71.5503931,
+        lat: 42.579962227255514
+    }, {
+        name: "Robison Conservation Area",
+        lng: -71.49799347,
+        lat: 42.59699989065713
+    }, {
+        name: "Rock Hill",
+        lng: -71.52728319,
+        lat: 42.58035261199077
+    }, {
+        name: "Rocky Hill Wildlife Sanctuary",
+        lng: -71.5422821,
+        lat: 42.57873169126811
+    }, {
+        name: "Sabine Memorial Woods",
+        lng: -71.59753561,
+        lat: 42.59864708368471
+    }, {
+        name: "Sampas Conservation Woods",
+        lng: -71.56721592,
+        lat: 42.64169245059861
+    }, {
+        name: "Sargisson Beach",
+        lng: -71.52013779,
+        lat: 42.58625313909901
+    }, {
+        name: "Sawtell Conservation Area",
+        lng: -71.54279709,
+        lat: 42.630007915799546
+    }, {
+        name: "Shattuck Homestead",
+        lng: -71.53395653,
+        lat: 42.621222436081446
+    }, {
+        name: "Shepley Hill",
+        lng: -71.57318115,
+        lat: 42.629896792599816
+    }, {
+        name: "Skinner Forest",
+        lng: -71.51048183,
+        lat: 42.59853284268783
+    }, {
+        name: "Skitapet Conservation Land",
+        lng: -71.52166128,
+        lat: 42.605683641466555
+    }, {
+        name: "Squannacook Picnic Site",
+        lng: -71.64358377,
+        lat: 42.62318002
+    }, {
+        name: "Still Meadow",
+        lng: -71.56270981,
+        lat: 42.64685505284199
+    }, {
+        name: "Stonebridge Conservation Area",
+        lng: -71.50852919,
+        lat: 42.57600873925143
+    }, {
+        name: "Surrenden Farms",
+        lng: -71.59657001,
+        lat: 42.58913087947743
+    }, {
+        name: "Taisey Conservation Restriction",
+        lng: -71.60094738,
+        lat: 42.611150097963694
+    }, {
+        name: "Taplin Wildlife Sanctuary",
+        lng: -71.51414037,
+        lat: 42.60777283850466
+    }, {
+        name: "Throne Hill",
+        lng: -71.62665367,
+        lat: 42.625472950886696
+    }, {
+        name: "Torrey Woods Conservation Area",
+        lng: -71.5408659,
+        lat: 42.63338718243275
+    }, {
+        name: "Unkety Brook Wildlife Management Area",
+        lng: -71.51627541,
+        lat: 42.64585407528064
+    }, {
+        name: "Walnut Run Conservation",
+        lng: -71.58895254,
+        lat: 42.61282026
+    }, {
+        name: "Watson Way Conservation Area",
+        lng: -71.55185223,
+        lat: 42.637634691903415
+    }, {
+        name: "Wattles Pond Conservation Area",
+        lng: -71.56663656,
+        lat: 42.654921747743686
+    }, {
+        name: "West Throne Hill Conservation Land",
+        lng: -71.63849831,
+        lat: 42.62812410202202
+    }, {
+        name: "Wharton Plantation",
+        lng: -71.52142525,
+        lat: 42.62557054622015
+    }, {
+        name: "Whispering Brook Conservation Area",
+        lng: -71.50784254,
+        lat: 42.64134545613525
+    }, {
+        name: "Wiewel Conservation Area",
+        lng: -71.55056477,
+        lat: 42.58936441483041
+    }, {
+        name: "Williams Barn and Sørhaug Woods",
+        lng: -71.5558219,
+        lat: 42.624497228179166
+    }, {
+        name: "Williams Conservation Land",
+        lng: -71.56350374,
+        lat: 42.62598079975534
+    }, {
+        name: "Woodland Park Conservation Area",
+        lng: -71.51118994,
+        lat: 42.58248441509994
+    }, {
+        name: "Woodland Pond Conservation Area",
+        lng: -71.50202751,
+        lat: 42.636610984286385
+    }
+];
